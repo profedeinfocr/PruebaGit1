70d59eaed5298e67037ea856ac78fc8a38e9753a
profe <profedeinfocr@gmail.com>
2022-09-27 21:55:16 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGit1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Hello world 0");
+            Console.WriteLine("Hello world 1");
+            Console.WriteLine("Hello world 2");
+            Console.WriteLine("Hello world 0");
+
+
+        }
+    }
+}
